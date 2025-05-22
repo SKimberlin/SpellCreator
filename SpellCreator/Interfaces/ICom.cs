cfd36ea81695b6bcaeea3a71cc366918a86c89af
SKimberlin <sethkimberlin04@gmail.com>
2025-05-21 23:09:45 -0600

    Create ICom.cs

---
+++ SpellCreator/Interfaces/ICom.cs
@@ -0,0 +1,14 @@
+﻿using SpellCreator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCreator.Interfaces
+{
+    interface ICom
+    {
+        void Execute();
+    }
+}
