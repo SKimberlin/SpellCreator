6b398a340948c693353158b10d10493fd30cc032
SKimberlin <sethkimberlin04@gmail.com>
2025-05-21 20:48:55 -0600

    Create SpellBuilder.cs

---
+++ SpellCreator/Model/SpellBuilder.cs
@@ -0,0 +1,51 @@
+﻿using SpellCreator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCreator.Model
+{
+    class SpellBuilder
+    {
+        private ISpell currentSpell;
+        private List<Type> addedEffects = new();
+
+        SpellBuilder()
+        {
+            currentSpell = new BaseSpell();
+        }
+
+        public bool AddEffect<T>() where T : SpellEffect
+        {
+            var type = typeof(T);
+
+            if (addedEffects.Contains(type))
+            {
+                // Use View to show a message that the effect is already added
+                return false;
+            }
+
+            var newSpell = Activator.CreateInstance(type, currentSpell) as T;
+
+            if (newSpell == null)
+            {
+                // Use View to show a message that the effect could not be added
+                return false;
+            }
+
+            currentSpell = newSpell;
+            addedEffects.Add(type);
+            return true;
+        }
+
+        public ISpell Builld()
+        {
+            var spell = currentSpell;
+            currentSpell = new BaseSpell();
+            addedEffects.Clear();
+            return spell;
+        }
+    }
+}
